3b0cb1cad894c53027407dbbda9fd85aaab4a8fb
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-14 15:13:01 +0530

    Create throwsekeyword.java

---
+++ throwsekeyword.java
@@ -0,0 +1,11 @@
+// we assuming our prgm might have exception
+import java.util.*;
+public class throwsekeyword {
+    public static void main(String args[])throws ArithmeticException
+    {
+        int a=10;
+        int b=0;
+        System.out.println(a/b);
+    }
+    
+}
